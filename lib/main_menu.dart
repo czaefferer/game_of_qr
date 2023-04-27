3c8e93835915a5faca0fe5ae37c581ed7cec2600
Christian Zaefferer <christian.zaefferer@uniteplus.de>
2023-04-27 08:50:58 +0200

    add license

--- lib/main_menu.dart
+++ lib/main_menu.dart
@@ -1,3 +1,8 @@
+/* This file is part of Game-of-QR.
+Game-of-QR is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Game-of-QR is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Game-of-QR. If not, see <https://www.gnu.org/licenses/>. */
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
